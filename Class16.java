3d1e7f7b14cb06a85fc8f25678a4857aece6a624
zack325546 <91444369+zack325546@users.noreply.github.com>
2022-01-15 22:07:41 +0800

    Create Class16.java

---
+++ Class16.java
@@ -0,0 +1,9 @@
+public class Class16{
+   public static void main(String args[])
+   {
+     String str="262904713";
+     int num;
+     num=Integer.parseInt(str);
+     System.out.println(num+"*2="+num*2);
+   }
+}
