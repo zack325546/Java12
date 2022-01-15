2004f40b22edb97c7a9eabff89f073dbe49d5982
zack325546 <91444369+zack325546@users.noreply.github.com>
2022-01-15 22:07:11 +0800

    Create Class15.java

---
+++ Class15.java
@@ -0,0 +1,10 @@
+public class Class15
+{
+   public static void main(String args[])
+   {
+     String str;
+     str=Float.toString(123.45f);
+     System.out.println(str);
+     System.out.println("length="+str.length());
+   }
+}
