c76fc14a24570e8748cea0ba9769b1626a6dac49
zack325546 <91444369+zack325546@users.noreply.github.com>
2022-01-15 22:08:07 +0800

    Create Class17.java

---
+++ Class17.java
@@ -0,0 +1,9 @@
+public class Class17{
+   public static void main(String args[])
+   {
+     String str="262904713";
+     short num;
+     num=Short.parseShort(str);
+     System.out.println(num+"*2="+num*2);
+   }
+}
