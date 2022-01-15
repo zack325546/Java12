145c403690d0566728f8ea392f337b621dcf8a0f
zack325546 <91444369+zack325546@users.noreply.github.com>
2022-01-15 22:06:20 +0800

    Create Class14.java

---
+++ Class14.java
@@ -0,0 +1,9 @@
+public class Class14
+{
+   public static void main(String args[])
+   {
+     String str="Habit is second nature.";
+     System.out.println("string="+str);
+     System.out.println("sub-string="+str.substring(16,22));
+   }
+}
